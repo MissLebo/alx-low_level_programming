23fca13bb70851a4973729bf413643d4453bca16
MissLebo <mslebobathebeng@gmail.com>
2023-02-16 15:59:40 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -this` program prints a line to the the standard error
- * return : 1
+ * main -this program prints a line to the the standard error
+ * Return: 1
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+return (1);
 }
