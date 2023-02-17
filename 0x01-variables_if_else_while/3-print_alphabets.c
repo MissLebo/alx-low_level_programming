a2ae7656bef4d3c5391a3b4d7989453c0bc29d0f
MissLebo <mslebobathebeng@gmail.com>
2023-02-17 15:30:42 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this program prints alaphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
