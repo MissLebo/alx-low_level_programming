88cbabdd5b6500ccf10d94ba26fd8ed640fe3722
MissLebo <mslebobathebeng@gmail.com>
2023-02-22 13:20:53 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,14 @@
 
 /**
  * print_last_digit - prints the last digit
- * @n: the number
- * Return: value of last digit
+ * @n: the number whose last digit is printed
+ * Return: value of last digit of @n
  */
 int print_last_digit(int n)
 {
 	int ld = n % 10;
 
-	if (ld < 0)
-		ld *= -1;
-
-	_putchar(ld + '0');
-
+ld = ld < 0 ? -1 * ld : ld;
+_putchar(ld + '0');
 	return (0);
 }
